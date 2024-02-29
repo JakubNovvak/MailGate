7a2fbfd3fe188c0aa24686ac86d42a057327d95a
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-29 22:58:00 +0100

    Create initial dtos

---
+++ Source/Server/MailGate.Server.Application/Dtos/ReadEmailEntryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailGate.Server.Application.Dtos
+{
+    public class ReadEmailEntryDto
+    {
+        int Id { get; set; }
+        bool WasSuccessfullySent { get; set; }
+        string WhenSubmitted { get; set; } = null!;
+        public string TargetEmail { get; set; } = null!;
+        public string MessageSubject { get; set; } = null!;
+        public string MessageContent { get; set; } = null!;
+    }
+}
