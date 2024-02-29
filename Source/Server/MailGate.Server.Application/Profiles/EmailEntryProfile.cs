80d28e12a7d9b3988f22887308d00e17efe019ca
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-29 22:58:10 +0100

    Create AutoMapper profile

---
+++ Source/Server/MailGate.Server.Application/Profiles/EmailEntryProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MailGate.Server.Application.Dtos;
+using MailGate.Server.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailGate.Server.Application.Profiles
+{
+    public class EmailEntryProfile: Profile
+    {
+        EmailEntryProfile() 
+        {
+            CreateMap<CreateEmailEntryDto, DbEntryEmail>();
+            CreateMap<DbEntryEmail, ReadEmailEntryDto>();
+        }
+    }
+}
