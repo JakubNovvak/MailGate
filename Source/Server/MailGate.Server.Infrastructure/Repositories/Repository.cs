3f572490a72fe6065a3a873947f70daa6fb050ca
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-01 00:21:17 +0100

    Create initial Repository with necessary data access functions

---
+++ Source/Server/MailGate.Server.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using MailGate.Server.Application.Dtos;
+using MailGate.Server.Domain.Entities;
+using MailGate.Server.Infrastructure.Presistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailGate.Server.Infrastructure.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly AppDbContext _dbContext;        
+
+        public Repository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreateEmailEntry(DbEntryEmail emailEntryToCreate)
+        {
+            if (emailEntryToCreate == null)
+                throw new ArgumentNullException();
+
+            _dbContext.Add(emailEntryToCreate);
+            SaveChanges();
+        }
+
+        public bool DoesEmailExist(int emailId)
+        {
+            if (_dbContext.dbEntryEmails.FirstOrDefault(email => email.Id == emailId) == null)
+                return false;
+
+            return true;
+        }
+
+        public IEnumerable<DbEntryEmail> GetAllEmailEntries()
+        {
+            var allDbEntryEmails = _dbContext.dbEntryEmails.ToList();
+
+            return allDbEntryEmails;
+        }
+
+        public DbEntryEmail? GetEmailEntry(int emailId)
+        {
+            return _dbContext.dbEntryEmails.FirstOrDefault(email => email.Id == emailId);
+        }
+
+        public bool SaveChanges()
+        {
+            return _dbContext.SaveChanges() == 0 ? false : true;
+        }
+
+
+
+        //TODO: Implement following functions
+        public IEnumerable<DbEntryEmail> GetAllEmailsByEmailAddress(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WasEmailAddressUsed(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
