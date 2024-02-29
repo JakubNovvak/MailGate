0643b4701d0d3e3607b838e6184a6849f3f1c29b
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-01 00:39:13 +0100

    Add new application's service files

---
+++ Source/Server/MailGate.Server.Application/Services/ITasksService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailGate.Server.Application.Dtos;
+
+namespace MailGate.Server.Application.Services
+{
+    public interface ITasksService
+    {
+        IEnumerable<ReadEmailEntryDto> GetAllEmailEntries();
+        ReadEmailEntryDto GetEmailEntry(int emailId);
+        void CreateEmailEntry(CreateEmailEntryDto emailEntryToCreate);
+    }
+}
