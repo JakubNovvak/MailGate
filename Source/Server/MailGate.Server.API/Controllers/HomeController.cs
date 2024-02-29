1cb369c51b9355579e065602e1653407785f136a
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-29 17:38:45 +0100

    Add Home Controller with testing endpoint

---
+++ Source/Server/MailGate.Server.API/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MailGate.Server.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController: ControllerBase
+    {
+        [HttpGet]
+        [Route("/helloworld", Name = "HelloWorldAction")]
+        public ActionResult<string> TestGet()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
