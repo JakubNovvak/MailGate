3d9fea0045c8ba1b7b40482c38e448210a1600c9
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-29 22:57:48 +0100

    Create email database record entity

---
+++ Source/Server/MailGate.Server.Domain/Entities/DbEntryEmail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailGate.Server.Domain.Entities
+{
+    public class DbEntryEmail
+    {
+        int Id { get; set; }
+        bool WasSuccessfullySent { get; set; }
+        DateTime WhenSubmitted { get; set; }
+        public string TargetEmail { get; set; } = null!;
+        public string MessageSubject { get; set; } = null!;
+        public string MessageContent { get; set; } = null!;
+    }
+}
