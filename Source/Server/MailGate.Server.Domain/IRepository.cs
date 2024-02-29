f3067df7aac4a710a13f9859331c07a6d6a8cde3
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-01 00:49:28 +0100

    Move IRepository file to the Domain project, to implement better hermetization fo the lower layers (especially data acess layer)

--- Source/Server/MailGate.Server.Domain/IRepository.cs
+++ Source/Server/MailGate.Server.Domain/IRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MailGate.Server.Application.Dtos;
 using MailGate.Server.Domain.Entities;
 
 namespace MailGate.Server.Infrastructure.Repositories
